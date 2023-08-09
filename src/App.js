29d48e7e99ef2907f1e876baef81bd7723f88436
harikesh <harikesh.linux@gmail.com>
2023-08-09 18:11:10 +0530

    my react page for practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+// import About from './components/About';
+import React, { useState } from 'react';
+
+// import Alert from './components/Alert';
+
+//react router 
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from "react-router-dom";
+
+
 
 function App() {
+
+
+  const [mode, setMode] = useState('light'); // whether dark mode is enable or not
+  // const [alert, setAlert] = useState(null);
+
+  // const showAlert = (message, type)=>{
+  //   setAlert({
+  //     msg: message,
+  //     type: type
+  //   })
+  // }
+
+  const toggleMode = ()=>{
+    if (mode === 'light'){
+      setMode ('dark')
+      document.body.style.backgroundColor = '#042743';
+      
+    }
+    else{
+      setMode ('light')
+      document.body.style.backgroundColor = 'white';
+      
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <>
+  <Navbar title="harikesh" mode={mode} toggleMode = {toggleMode}/>
+
+  {/* <Alert alert = {alert}/> */}
+
+  <div className="container">
+    {/* <Router>
+  <Routes>
+          <Route exact path="/about"
+            element={<About/>} />
+          
+          
+          <Route exact path="/"
+         element ={ } /> */}
+        
+    {/* </Routes> */}
+
+    <TextForm heading="Convert your text" mode={mode}/>
+
+{/* <About/> */}
+        {/* </Router> */}
+</div>
+
+    </>
+
   );
 }
 
 export default App;
+
---
+++ src/components/TextForm.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    // console.log("uppercase was clicked");
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleLoClick = () => {
+    // console.log("uppercase was clicked");
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+   const handleClsClick = () => {
+    // console.log("uppercase was clicked");
+    let newText = '';
+    setText(newText);
+  };
+
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    // text.select();
+    navigator.clipboard.writeText(text.value)
+  };
+
+
+  const handleOnChange = (event) => {
+    // console.log("On change");
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("");
+  // setText = ("new text");
+  return (
+    <>
+    <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}} >
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          id="myBox"
+          rows="8"
+          style={{backgroundColor: props.mode==='dark'?'grey':'white',
+          color: props.mode==='dark'?'white':'#042743'}}
+        ></textarea>
+      </div>
+      <button className="btn btn-primary" onClick={handleUpClick}>
+        Convert to Uppercase
+      </button>
+      <button className="btn btn-success mx-2" onClick={handleLoClick}>
+        Convert to LowerCase
+      </button>
+
+      <button className="btn btn-danger mx-2" onClick={handleCopy}>
+        Copy Text
+      </button>
+
+       <button className="btn btn-danger mx-2" onClick={handleClsClick}>
+        Clear Text
+      </button>
+      
+
+    </div>
+
+    <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length -1} words, {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text .length>0?text:"Enter something in the textbox to preview"}</p>
+    </div>
+    </>
+  );
+}
